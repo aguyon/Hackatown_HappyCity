fa19fa2e0daa3b29e5f629748e9112be7f5ca189
Benoit Briastre <benoit.briastre@gmail.com>
2019-06-26 17:44:36 +0200

    oups

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Home extends Component {
+class Profil extends Component {
   constructor(props) {
     super(props);
     this.state = {}
@@ -14,4 +14,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default Profil;
