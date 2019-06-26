27ecc1b3ba9c6956786230bfde6c2b054a18580a
Benoit Briastre <benoit.briastre@gmail.com>
2019-06-26 20:51:04 +0200

    mise en place actu

--- src/pages/Actualite.jsx
+++ src/pages/Actualite.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
+import Grid from '@material-ui/core/Grid';
+import RecipeReviewCard from '../components/Actucards';
 
 class Actualite extends Component {
   constructor(props) {
@@ -6,9 +9,33 @@ class Actualite extends Component {
     this.state = {}
   }
   render() {
-    return (  
-      <div>
-        Coucou
+    return (
+      <div className="actupage">
+        <Grid
+          item xs={12}
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center">
+          <DeleteRoundedIcon />
+          <DeleteRoundedIcon />
+        </Grid>
+        <Grid
+          item xs={12}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center">
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+          <RecipeReviewCard />
+        </Grid>
       </div>
     );
   }
