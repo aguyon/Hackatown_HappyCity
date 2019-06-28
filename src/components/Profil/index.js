0191325991c8126ae8a68172457aca5d10445dcc
aguyon <alexisguyon01@gmail.com>
2019-06-28 11:31:40 +0200

    css

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import withContext from '../Context/withContext';
+import axios from 'axios';
 
 class Profil extends React.Component {
   constructor(props) {
@@ -8,31 +9,12 @@ class Profil extends React.Component {
     this.state = {};
   }
 
-  componentWillReceiveProps() {
-    
-  }
-
   render() {
     const { userInfo } = this.props;
     return (
       <div>
         <h2 className="title">My Profil</h2>
-        <div className="infoUser">
-          <h3 className="problemes">Issues Reported</h3>
-          {userInfo.issues.map((issue, i) => (
-            <div key={`issue-${i + 1}`}>
-              <p className="infoIssues">{issue.type.name}</p>
-              <p className="infoIssues">{issue.score}</p>
-              <p className="infoIssues">{issue.status}</p>
-              <img
-                src={`./assets/${issue.type.name}.png`}
-                className="issuesImg"
-                alt=""
-              />
-            </div>
-          ))}
-          <h3 className="email">Informations</h3>
-        </div>
+        <h3 className="email">Informations</h3>
         <p className="info">
           Username :
           {' '}
@@ -43,6 +25,19 @@ class Profil extends React.Component {
           {' '}
           {userInfo ? userInfo.email : null}
         </p>
+        <div className="infoUser">
+          <h3 className="problemes">Issues Reported</h3>
+          {userInfo.issues.map((issue, i) => (
+            <div key={`issue-${i + 1}`} className="encartIssues">
+              <h4 className="infoIssuesName">{issue.type.name}</h4>
+              <img
+                src={`./assets/${issue.type.name}.png`}
+                className="issuesImg"
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
