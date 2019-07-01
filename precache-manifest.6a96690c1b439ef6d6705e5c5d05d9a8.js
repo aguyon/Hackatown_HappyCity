7f1f5488381b4ca8d315f99889abd0fc186f67e0
aguyon <alexisguyon01@gmail.com>
2019-07-01 11:16:00 +0200

    Updates

---
+++ precache-manifest.6a96690c1b439ef6d6705e5c5d05d9a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4480fbe2206810a3807fc9a1a6d5b66e",
+    "url": "/Hackatown_HappyCity/index.html"
+  },
+  {
+    "revision": "a7c6b84824491e1f7a72",
+    "url": "/Hackatown_HappyCity/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "822ecc45c439c261194e",
+    "url": "/Hackatown_HappyCity/static/css/main.8c1089a7.chunk.css"
+  },
+  {
+    "revision": "a7c6b84824491e1f7a72",
+    "url": "/Hackatown_HappyCity/static/js/2.7c09e611.chunk.js"
+  },
+  {
+    "revision": "822ecc45c439c261194e",
+    "url": "/Hackatown_HappyCity/static/js/main.5ede109e.chunk.js"
+  },
+  {
+    "revision": "f8f84f8f81ab9dc28a28",
+    "url": "/Hackatown_HappyCity/static/js/runtime~main.30c438c9.js"
+  },
+  {
+    "revision": "74168996017db1eae99d8ba8b9cd4093",
+    "url": "/Hackatown_HappyCity/static/media/Electric.74168996.png"
+  },
+  {
+    "revision": "0283bf124698412de407522f0e44a3b5",
+    "url": "/Hackatown_HappyCity/static/media/HappyCityFullLogo.0283bf12.png"
+  },
+  {
+    "revision": "2e74f46346f5ff862d479b563202a3d1",
+    "url": "/Hackatown_HappyCity/static/media/HappyCityLogo.2e74f463.png"
+  }
+]);
